f916b910b1ad050e160513cdccd40d48f91176b6
huangzq <huangzq11>
2019-07-30 14:02:36 +0800

    add

--- cache/log/201907/30.php
+++
@@ -1,7 +0,0 @@
-
-{"ip":"127.0.0.1","uid":0,"time":1564453089,"action":"密码不正确（密码123qwe）","username":"admin"}
-{"ip":"127.0.0.1","uid":0,"time":1564453093,"action":"密码不正确（密码123456）","username":"admin"}
-{"ip":"127.0.0.1","uid":0,"time":1564453098,"action":"登录后台成功","username":"admin"}
-{"ip":"127.0.0.1","uid":1,"time":1564453474,"action":"设置网站参数","username":"admin"}
-{"ip":"127.0.0.1","uid":1,"time":1564453521,"action":"设置网站参数","username":"admin"}
-{"ip":"127.0.0.1","uid":1,"time":1564453540,"action":"设置网站参数","username":"admin"}
