a1aac58cce1cd68d58d23aaffe617bbe320e8e9f
huangzq <huangzq11>
2019-07-30 14:13:27 +0800

    add

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'xiaoshuo';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = '010xr.com';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'gbk';
+
+
+?>
